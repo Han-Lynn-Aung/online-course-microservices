bd616e7710f311248601e11c296462731ac085fa
Han-Lynn-Aung <hanlynnaung1997@gmail.com>
2024-01-30 23:37:42 +0630

    intial commit

---
+++ src/main/java/com/natrix/onlinecoursemicroservice/OnlineCourseMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.natrix.onlinecoursemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCourseMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineCourseMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/natrix/onlinecoursemicroservice/OnlineCourseMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natrix.onlinecoursemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCourseMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
